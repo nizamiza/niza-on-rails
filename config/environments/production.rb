282d574c777753962e0fe6c616d1486a28e3433e
Niza Toshpulatov <niza@hey.com>
2024-04-19 04:40:04 +0500

    Update production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "niza_on_rails_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.mail.me.com",
+    port: 587,
+    domain: "niza.cz",
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    authentication: "plain",
+    enable_starttls: true,
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

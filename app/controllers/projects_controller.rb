d6c91208136c94770b87880128d9c4b5ac6b8cde
Niza Toshpulatov <niza@hey.com>
2024-04-30 14:08:47 +0200

    Complete projects section

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,19 +2,29 @@ class ProjectsController < ApplicationController
   def index
     @projects = [
       {
-        title: "Using PocketBase, Deno, and Fresh to deploy a production ready app under an hour",
-        repo_url: "https://github.com/nizamiza/noc-pb-deno-fresh-workshop",
-        cover_image_path: view_context.image_path("projects/noc-workshop.jpeg"),
+        title: "Niza on Rails - This very website!",
+        repo_url: "https://github.com/nizamiza/niza-on-rails",
+        cover_image_path: view_context.image_path("projects/niza-on-rails.webp"),
       },
       {
-        title: "ZodEnv - Zod powered environment variable parser",
+        title: "Lo-cal - Simple calendar that stores everything locally",
+        repo_url: "https://github.com/nizamiza/lo-cal",
+        cover_image_path: view_context.image_path("projects/lo-cal.webp"),
+      },
+      {
+        title: "PocketBase, Deno, Fresh Workshop",
+        repo_url: "https://github.com/nizamiza/noc-pb-deno-fresh-workshop",
+        cover_image_path: view_context.image_path("projects/noc-workshop.webp"),
+      },
+      {
+        title: "ZodEnv - Zod powered ENV file parser",
         repo_url: "https://github.com/nizamiza/zodenv",
-        cover_image_path: view_context.image_path("projects/zodenv.jpeg"),
+        cover_image_path: view_context.image_path("projects/zodenv.webp"),
       },
       {
         title: "RPM - A rudimentary plugin manager for NeoVim",
         repo_url: "https://github.com/nizamiza/rpm",
-        cover_image_path: view_context.image_path("projects/rpm.jpeg"),
+        cover_image_path: view_context.image_path("projects/rpm.webp"),
       },
     ]
   end

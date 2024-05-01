62ba027931da6c0915b2dc143500ea3520cb63ef
Niza Toshpulatov <niza@hey.com>
2024-05-01 09:33:30 +0200

    Fix demos, reorder projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,11 +2,6 @@ class ProjectsController < ApplicationController
   def index
     @projects = [
       {
-        title: "Niza on Rails - This very website!",
-        repo_url: "https://github.com/nizamiza/niza-on-rails",
-        cover_image_path: view_context.image_path("projects/niza-on-rails.webp"),
-      },
-      {
         title: "Lo-cal - Simple calendar that stores everything locally",
         repo_url: "https://github.com/nizamiza/lo-cal",
         cover_image_path: view_context.image_path("projects/lo-cal.webp"),
@@ -15,6 +10,11 @@ class ProjectsController < ApplicationController
         title: "PocketBase, Deno, Fresh Workshop",
         repo_url: "https://github.com/nizamiza/noc-pb-deno-fresh-workshop",
         cover_image_path: view_context.image_path("projects/noc-workshop.webp"),
+      },
+      {
+        title: "Niza on Rails - This very website!",
+        repo_url: "https://github.com/nizamiza/niza-on-rails",
+        cover_image_path: view_context.image_path("projects/niza-on-rails.webp"),
       },
       {
         title: "ZodEnv - Zod powered ENV file parser",
